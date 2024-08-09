57846ce1de73ec319447e2240219c15b7f87c909
sela-xyz <betsela123@gmail.com>
2024-08-09 11:39:01 +0700

    Use environment variable

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-
 //MIDDLEWARE
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
-const port = 3000;
+
+
+const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
-  });
-  
+  console.log(`LISTEN ON PORT: ${port}!`);
+});
