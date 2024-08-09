fc1b09375db894c4be04c9db99fbac34bb2727f7
sela-xyz <betsela123@gmail.com>
2024-08-09 10:20:36 +0700

    Use middleware chaining concept

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,12 +2,19 @@ const express = require('express');
 const tourController = require('../controllers/tourControllers');
 const router = express.Router();
 
-const { getAllTour, getTour, createTour, updateTour, deleteTour, checkId } =
-  tourController;
+const {
+  getAllTour,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+  checkId,
+  checkBody,
+} = tourController;
 
 router.param('id', checkId);
 
-router.route('/').get(getAllTour).post(createTour);
+router.route('/').get(getAllTour).post(checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
